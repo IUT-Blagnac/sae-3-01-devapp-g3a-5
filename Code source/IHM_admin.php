6d8cba504127c7b913da1544c5fa7242c9d76908
L-demany <loelia.dem@gmail.com>
2024-01-09 16:08:41 +0100

    squelette code

---
+++ Code source/IHM_admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Chasse au trésor : interface administrateur</title>
+</head>
+<body>
+		Bienvenue sur l'interface administrateur !
+</body>
+</html>
+
