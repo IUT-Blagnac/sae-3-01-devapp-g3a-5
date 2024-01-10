d6bb6654808de763d7ef9625bcf7e40b8c1dd782
L-demany <loelia.dem@gmail.com>
2024-01-10 11:44:10 +0100

    test ihm admin

--- Code source/IHM_admin.php
+++ Code source/IHM_admin.php
@@ -5,7 +5,32 @@
 	<title>Chasse au trésor : interface administrateur</title>
 </head>
 <body>
-		Bienvenue sur l'interface administrateur !
+		<h2>Bienvenue sur l'interface administrateur !</h2>
+		<button> Message </button>
+
+		<table>
+			<tr>
+				<td rowspan ="4"> Equipe A </td>
+				<td> Id Capteur </td>
+				<td> Trouvé ? </td>
+				<td> Tps </td>
+			</tr>
+			<tr>
+				<td> 123 </td>
+				<td> V </td>
+				<td> 00:45 </td>
+			</tr>
+			<tr>
+				<td> 456 </td>
+				<td> V </td>
+				<td> 01:26 </td>
+			</tr>
+			<tr>
+				<td> 127893 </td>
+				<td>  </td>
+				<td> --:-- </td>
+			</tr>
+		</table>
 </body>
 </html>
 
