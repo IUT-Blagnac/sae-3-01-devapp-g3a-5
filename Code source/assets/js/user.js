53e8239f1b2699f2715a23ebb7cb646bc938d8af
thomasduppi <126145508+thomasduppi@users.noreply.github.com>
2024-01-12 10:19:04 +0100

    génération aléatoire des couleurs en fonction du nombre de joueurs + attribution des couleurs aux joueurs

--- Code source/assets/js/user.js
+++ Code source/assets/js/user.js
@@ -1,1 +1,9 @@
-// code pour la page user
+jQuery(document).ready(function() {
+    var idPlayers = ['0xFD24', '0xFD25', '0xFD26', '0xFD27'];
+    var players = {};
+
+    idPlayers.forEach(function(player) {
+        players[player] = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+    });
+});
+
